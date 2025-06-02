98079c1877bb976d172dadc678bf97e23bf42ada
Karan-Bharti1 <karan.codes1@gmail.com>
2025-06-02 13:47:12 +0530

    feat: added useFetch as an util and displayed album data

--- src/pages/ViewAlbum.jsx
+++ src/pages/ViewAlbum.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import Header from '../../components/Header'
 import { useParams } from 'react-router-dom'
-
+import useFetch from '../utils/useFetch'
+import { baseURL } from '../url'
+import { Link } from 'react-router-dom'
 function ViewAlbum() {
     const {albumId}=useParams()
     console.log(albumId)
+
+    const {data,loading,error}=useFetch(`${baseURL}/albums/album/${albumId}`)
+    console.log(data)
   return (<div className='container mt-3'>
     <Header/>
     <main className='container mt-3'>
-
+      <div className='view-album-head'>
+        <Link to="/dashboard" className="button-create-album text-decoration-none"> Back to Dashboard</Link>
+      </div>
+{!loading && <>
+<h1 className='text-center mt-4'>{data.name}</h1>
+{data.description.length>0 && <>
+<h4 className='text-center mt-3'>{data .description}</h4></>}
+</>}
     </main>
 </div>
   )
